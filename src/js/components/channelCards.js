84ff52e7ba3f0b9a66efb7b531a6a8c93ed59b6e
proAlvaroSam <pro.alvaro.sam@gmail.com>
2025-02-14 20:33:41 -0300

    logo do site

--- src/js/components/channelCards.js
+++ src/js/components/channelCards.js
@@ -4,7 +4,7 @@ const channelsData = [
         id: 'filipe-deschamps',
         name: "Filipe Deschamps",
         avatar: "https://embed.filekitcdn.com/e/iajLrvtJnG9GL5FWs5H8a1/6M4He65n8bpXQJNcxeLXLL",
-        subscribers: "+1M",
+        subscribers: "+700k",
         description: {
             'pt': "Vídeos sobre tecnologia, programação e empreendedorismo digital com um toque de humor e autenticidade.",
             'en': "Videos about technology, programming and digital entrepreneurship with a touch of humor and authenticity."
@@ -15,7 +15,7 @@ const channelsData = [
         id: 'curso-em-video',
         name: "Curso em Vídeo",
         avatar: "https://yt3.ggpht.com/a/AATXAJxPC_D40WpkAx7kkduf4csBFe0tqY5SpoGs8w=s900-c-k-c0xffffffff-no-rj-mo",
-        subscribers: "+3M",
+        subscribers: "+2M",
         description: {
             'pt': "O Curso em Vídeo é um projeto criado pelo Professor Gustavo Guanabara que disponibiliza cursos de tecnologia gratuitos e de qualidade para todo mundo.",
             'en': "Curso em Vídeo is a project created by Professor Gustavo Guanabara that provides free and quality technology courses for everyone."
